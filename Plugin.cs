695e7ebf189e81438337f06def5fdbf12f00005b
defaultuser0 <axolotlslmao@gmail.com>
2024-12-14 17:08:59 -0600

    kinda fixed the formatting

--- Plugin.cs
+++ Plugin.cs
@@ -15,38 +15,38 @@ namespace legs
 
 		void Start()
 		{
-            Newtilla.Newtilla.OnJoinModded += OnModdedJoin;
-            Newtilla.Newtilla.OnLeaveModded += OnModdedLeave;
-            HarmonyPatches.ApplyHarmonyPatches();
-			GorillaTagger.OnPlayerSpawned(Initialized);
+            	Newtilla.Newtilla.OnJoinModded += OnModdedJoin;
+           	Newtilla.Newtilla.OnLeaveModded += OnModdedLeave;
+            	HarmonyPatches.ApplyHarmonyPatches();
+		GorillaTagger.OnPlayerSpawned(Initialized);
 		}
 
 		void Initialized()
 		{
-        }
+        	}
 
 		[ModdedGamemodeJoin]        
 		public void OnModdedJoin(string gamemode)
 		{
-            GameObject arm1 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.R/");
-            arm1.transform.localPosition = new Vector3(-0.0183f, 0.0759f, 0.0791f);
-            GameObject arm2 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.L/");
-            arm2.transform.localPosition = new Vector3(-0.0183f, 0.0759f, 0.0791f);
-            GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(1.73f, 1.73f, 1.73f);
-            Debug.Log("Enabled Legs");
-            inRoom = true;
+           	GameObject arm1 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.R/");
+           	arm1.transform.localPosition = new Vector3(-0.0183f, 0.0759f, 0.0791f);
+            	GameObject arm2 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.L/");
+            	arm2.transform.localPosition = new Vector3(-0.0183f, 0.0759f, 0.0791f);
+            	GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(1.73f, 1.73f, 1.73f);
+            	Debug.Log("Enabled Legs");
+            	inRoom = true;
 		}
 
 		[ModdedGamemodeLeave]
 		public void OnModdedLeave(string gamemode)
 		{
-            GameObject arm1 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.R/");
-            arm1.transform.localPosition = new Vector3(-0.0183f, 0.3432f, 0.0791f);
-            GameObject arm2 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.L/");
-            arm2.transform.localPosition = new Vector3(-0.0183f, 0.3432f, 0.0791f);
-            GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(1f, 1f, 1f);
-            Debug.Log("Disabled Legs");
-            inRoom = false;
+            	GameObject arm1 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.R/");
+            	arm1.transform.localPosition = new Vector3(-0.0183f, 0.3432f, 0.0791f);
+           	GameObject arm2 = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/RigAnchor/rig/body/shoulder.L/");
+            	arm2.transform.localPosition = new Vector3(-0.0183f, 0.3432f, 0.0791f);
+            	GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(1f, 1f, 1f);
+            	Debug.Log("Disabled Legs");
+            	inRoom = false;
 		}
 	}
 }
